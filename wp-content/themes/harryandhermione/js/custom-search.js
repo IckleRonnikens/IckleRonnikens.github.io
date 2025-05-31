b1069d9658e093af24002ebad7bd436b8f904628
IckleRonnikens <76468847+IckleRonnikens@users.noreply.github.com>
2025-05-31 23:17:15 +1000

    upload

---
+++ wp-content/themes/harryandhermione/js/custom-search.js
@@ -0,0 +1,7 @@
+$("input#keyword").keyup(function() {
+      if ($(this).val().length > 2) {
+        $("#datafetch").show();
+      } else {
+        $("#datafetch").hide();
+      }
+    });
---
+++ wp-content/themes/harryandhermione/js/scroll.js
@@ -0,0 +1,20 @@
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    delay: 0
+
+};
+
+ScrollReveal().reveal('.slide-up', slideUp, { mobile: false });
+
+var slideUp2 = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    delay: 500
+
+};
+
+ScrollReveal().reveal('.slide-up2', slideUp2, { mobile: false });
+
